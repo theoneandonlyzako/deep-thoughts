3f8813a013be12ed210d84bdf6c317f201fdefcf
Zak Stamps <theoneandonlyzako@gmail.com>
2021-08-05 02:42:23 -0700

    added thought query to profile and dashboard

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -58,6 +58,7 @@ const Profile = () => {
           </button>
         )}
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
@@ -72,7 +73,7 @@ const Profile = () => {
           />
         </div>
       </div>
-      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
+
     </div>
   );
 };
